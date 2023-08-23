ca010d8912868d4a4f79780ba4191cd778934091
DevTiw37 <tiwaridevesh937@gmail.com>
2023-08-23 20:11:38 +0530

    new

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-//	this program was written by Devesh Tiwari
-/*This is
-a 
-multiline
-comment */
-    cout<<"Hello world";
-    return 0;
+int main() {
+  //	this program was written by Devesh Tiwari
+  /*This is
+  a
+  multiline
+  comment */
+  cout << "Hello world";
+  return 0;
 }
